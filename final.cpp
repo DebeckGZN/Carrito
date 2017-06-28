6cb2ec753b40df62e529b17f92e778af0b68d93c
Thaylo <thaylogzn@gmail.com>
2017-06-28 17:29:54 -0300

    Teste

---
+++ final.cpp
@@ -0,0 +1,27 @@
+#include "parser.cpp"
+#include "funcs.h"
+
+
+int main(int argc, char *argv[])
+{
+
+        parseXML(argv[1]);
+        glutInit( &argc, argv );
+    	glutInitDisplayMode( GLUT_DOUBLE | GLUT_RGB );
+    	glutInitWindowSize( maior.raio*2, maior.raio*2 );
+    	glutInitWindowPosition( 70, 75 );
+    	glutCreateWindow("circuito");
+
+    	init();
+  		glutDisplayFunc( display );
+        glutKeyboardFunc(keyPressed);
+        glutKeyboardUpFunc(keyUp);
+        glutPassiveMotionFunc( passiveMotion );
+        glutMouseFunc( mouse );
+
+    	glutMainLoop();
+    	return 0;
+}
+
+
+
